f809928a4a5714e701d2b59f4a83c5ac48c44dd1
HsiaRepo <logan@hsia.us>
2023-10-31 15:52:33 -0400

    feat(controller): changed index response to get from $messages array. added attribute to get one entry from $message

--- symfony-hands-on/src/Controller/HelloController.php
+++ symfony-hands-on/src/Controller/HelloController.php
@@ -7,10 +7,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+
+    private array $messages = [
+        "HI!",
+        "Hello!",
+        "Howdy!",
+        "Hey there!"
+    ];
+
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return new Response("HI!");
+        return new Response(implode(', ', $this->messages));
+    }
+
+    #[Route('/messages/{id}')]
+    public function showOne($id): Response
+    {
+        return new Response($this->messages[$id]);
     }
     
 }
