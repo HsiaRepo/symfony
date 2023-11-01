f222763d652382d4eb9bfc1cf8fe3abceab94103
HsiaRepo <logan@hsia.us>
2023-11-01 16:16:39 -0400

    feat(twig): install twig packages from composer

--- symfony-hands-on/var/cache/dev/url_generating_routes.php
+++ symfony-hands-on/var/cache/dev/url_generating_routes.php
@@ -4,4 +4,6 @@
 
 return [
     '_preview_error' => [['code', '_format'], ['_controller' => 'error_controller::preview', '_format' => 'html'], ['code' => '\\d+'], [['variable', '.', '[^/]++', '_format', true], ['variable', '/', '\\d+', 'code', true], ['text', '/_error']], [], [], []],
+    'app_index' => [['limit'], ['limit' => '3', '_controller' => 'App\\Controller\\HelloController::index'], [], [['variable', '/', '[^/]++', 'limit', true]], [], [], []],
+    'app_show_one' => [['id'], ['_controller' => 'App\\Controller\\HelloController::showOne'], ['id' => '\\d+'], [['variable', '/', '\\d+', 'id', true], ['text', '/messages']], [], [], []],
 ];
